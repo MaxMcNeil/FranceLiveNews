2fb2104ef43d5f6aaa714d215d5d05edd6e33218
MaxMcNeil <72625668+MaxMcNeil@users.noreply.github.com>
2026-07-06 22:26:07 +0200

    Update tags.js

--- js/tags.js
+++ js/tags.js
@@ -1,1 +1,23 @@
+export function getTags(text = "") {
+  const t = text.toUpperCase();
 
+  const tags = [];
+
+  if (/(MEURTRE|ASSASSIN|VIOL|FUSILLADE|CRIME)/.test(t)) {
+    tags.push("CRIME");
+  }
+
+  if (/(FOOT|COUPE DU MONDE|LIGUE|MATCH)/.test(t)) {
+    tags.push("SPORT");
+  }
+
+  if (/(GOUVERN|POLICE|MINISTRE|ÉLECTION|PARLEMENT)/.test(t)) {
+    tags.push("POLITIQUE");
+  }
+
+  if (/(UKRAINE|GUERRE|RUSSIE|ISRAEL|GAZA)/.test(t)) {
+    tags.push("GÉOPOLITIQUE");
+  }
+
+  return tags.length ? tags : ["INFO"];
+}
