54335e1b37767bc3b86196a645cf6efc1c3743cf
MaxMcNeil <72625668+MaxMcNeil@users.noreply.github.com>
2026-07-08 13:51:51 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,13 +1,13 @@
 export const RSS_FEEDS = [
-
-"https://www.franceinfo.fr/titres.rss",
-
-"https://www.lefigaro.fr/rss/figaro_actualites.xml",
-
-"https://www.20minutes.fr/feeds/rss-une.xml",
-
-"https://www.leparisien.fr/actualites-a-la-une/rss.xml"
-
+    "https://www.franceinfo.fr/titres.rss",
+    "https://www.lefigaro.fr/rss/figaro_actualites.xml",
+    "https://www.20minutes.fr/feeds/rss-une.xml",
+    "https://www.leparisien.fr/actualites-a-la-une/rss.xml",
+    "https://www.sudouest.fr/faits-divers/rss.xml",
+    "https://www.ledauphine.com/Faits-divers-Justice/rss",
+    "https://www.cnews.fr/rss/categorie/faits%20divers",
+    "https://rmccrime.bfmtv.com/rss/affaires-criminelles/france/",
+    "https://rmccrime.bfmtv.com/rss/affaires-criminelles/"
 ];
 
 export const KEYWORDS=[
